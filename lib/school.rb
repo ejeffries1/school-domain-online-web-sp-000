655b297195dd6cb3f57a7902dd13f6256a2e9649
Evan Jeffries <ejeffries14@gmail.com>
2021-05-24 07:19:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+  attr_accessor :roster, :name, :length, :grade
+  
+  def initialize(name)
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    if @roster[grade]
+      @roster[grade] << student
+    else
+    @roster[grade] = [student]
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.each do |grade, student|
+       binding.pry
+    end
+  end
+end
